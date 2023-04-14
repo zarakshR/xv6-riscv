d8e2c5c5e8a794319712507d5a88906571fe178b
Zaraksh Rahman <zarakshR@pm.me>
2023-04-14 22:28:08 +0100

    Minor changes Remove random tab character from user/usys.pl Remove needless linebreak in kernel/syscall.c

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -140,8 +140,7 @@ syscall(void)
     // and store its return value in p->trapframe->a0
     p->trapframe->a0 = syscalls[num]();
   } else {
-    printf("%d %s: unknown sys call %d\n",
-            p->pid, p->name, num);
+    printf("%d %s: unknown sys call %d\n", p->pid, p->name, num);
     p->trapframe->a0 = -1;
   }
 }
